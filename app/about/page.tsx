12114a0237b8e74e1329ca46a8c4aa262f545e31
Leopoldo Garcia <legarbo@gmail.com>
2024-11-04 14:35:24 -0500

    add Google as provider

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Link href="/pricing">Pricing 🍺</Link>
+      <h1>This is the About Page</h1>)
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 
 export default function HomePage() {
-  <>
-    <h1>This is the Home Page</h1>
-    <Link href="/pricing">Pricing 🍺</Link>
-  </>;
+  return (
+    <>
+      <h1>This is the Home Page</h1>
+      <Link href="/pricing">Pricing 🍺</Link>)
+    </>
+  );
 }
